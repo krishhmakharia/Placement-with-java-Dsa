b59a8d4d7901775a3afa2e921607110e61f5d668
krish <krishhmakharia@gmail.com>
2025-05-17 22:26:31 +0530

    Update BasicSorting.java

--- src/BasicSorting.java
+++ src/BasicSorting.java
@@ -14,13 +14,52 @@ public class BasicSorting {
         System.out.print("\nAfter : ");
         printArray(arr);
     }
+    public static void selectionSort(int[] arr){
+        System.out.print("Before : ");
+        printArray(arr);
+        for (int i=0; i<arr.length-1; i++){
+            int min_pos=i;
+            for (int j=i+1; j<arr.length; j++){// its will check the value up to last element
+                if(arr[min_pos]>arr[j]){
+                    min_pos = j;
+                }
+            }
+            //swap
+            int temp = arr[min_pos];
+            arr[min_pos] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.print("\nAfter : ");
+        printArray(arr);
+    }
     public static void printArray(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+"\t");
         }
     }
+    public static void insertionSort(int[] arr){
+        System.out.print("Before : ");
+        printArray(arr);
+        for(int i=1; i<arr.length; i++){//here we assume that arr[0] is sorted array
+            int curr=arr[i];
+            int pre_pos=i-1;
+            while(pre_pos>=0 && arr[pre_pos]>curr){
+                arr[pre_pos+1]=arr[pre_pos];
+                pre_pos--;
+            }
+            //insertion of curr at its right postion
+            arr[pre_pos+1]=curr;
+
+        }
+        System.out.print("\nAfter : ");
+        printArray(arr);
+    }
     public static void main(String [] args){
         int [] arr ={1,2,55,76,90,100,7};
-        bubbleSort(arr);
+//        bubbleSort(arr);
+//        selectionSort(arr);
+        insertionSort(arr);
+
+        
     }
 }
